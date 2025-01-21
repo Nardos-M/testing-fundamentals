55375dc912cb9e2bd34601381dabe7b936c667f8
shaneinceptionu <shane@inceptionu.com>
2025-01-20 20:05:22 -0700

    added TDD example

---
+++ tddExamples/validatePasswordTDD.js
@@ -0,0 +1,16 @@
+// TDD Exercise: Validate a Password
+// Request:
+// Write a JavaScript function that validates a password.
+//
+// Acceptance Criteria:
+// Should return "valid" if the password meets all criteria:
+// At least 8 characters long.
+// Contains at least one uppercase letter.
+// Contains at least one lowercase letter.
+// Contains at least one number.
+// Should return "invalid" if the password does not meet any of the criteria.
+// Should return "invalid input" if the input is not a string.
+
+//1. Write the tests first (Red) i.e. validatePassword.test.js
+//2. Write the Minimal Code to Pass the Tests (Green) i.e. validatePassword.js
+//3. Add Edge Case Tests and Refactor (and apply dependency injections)
